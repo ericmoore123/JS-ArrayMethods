0065bab7fc78d10fd55b68e5ca13b7ddb9a2ba93
Eric Moore <ericmoore1999@outlook.com>
2021-09-16 14:12:11 -0400

    added simple methods

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,6 +1,20 @@
-const names = ["Jake", "Martin", "Eric", "Angie", "Malcolm"];
+let names = ["Jake", "Martin", "Eric", "Angie", "Malcolm"];
+
+// Simple Methods
+let joined = names.join(" "); // Joins each element in array with a space between each item
+console.log(joined);
+
+let popped = console.log(names.pop()); // Removes and returns last element from an array
+popped;
+
+let pushed = console.log(names.push("Malcolm")); // Adds a new element to end of array, returns new count of array
+pushed;
+
+let shifted = console.log(names.shift()); // Removes first element of array and shift existing down 1
+shifted;
 
 names.map((currentElement) => { // Optional elements include Index: spot of current elem, and an arr: hold the array
     // Calls a defined callback function on each item in an array
     console.log(currentElement + " :)");
 });
+
